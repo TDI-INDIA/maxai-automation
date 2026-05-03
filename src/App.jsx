94b141527995ed1dc3dd48f3c59f04a7e5f7acb3
Abhishek V <tdicommons@gmail.com>
2026-05-03 21:23:36 +0530

    Push src/App.jsx file content on 2026-05-03 15:53:21 UTC

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <HelmetProvider>
+      <Router>
+        <Navigation />
+        <main role="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </HelmetProvider>
+  );
+}
+
+export default App;
